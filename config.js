11b80dcd753b6ec0dd5852c2c77990109b0af137
Victor-Duong <Giathang01234@gmail.com>
2025-03-07 20:45:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const CONFIG = {
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Đằng sau chữ yêu đây là thương
+    Chúc em 8/3 này vui vẻ và hạnh phúc nơi anh`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
